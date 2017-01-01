69d9c8cbd13fc7668fac56501421ac4b71337d68
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-01-01 17:08:27 -0500

    added pokemon class

---
+++ pokemonTypeMatchups/pokemonTypeMatchups/Pokemon.swift
@@ -0,0 +1,19 @@
+//
+//  Pokemon.swift
+//  pokemonTypeMatchups
+//
+//  Created by Marty Avedon on 1/1/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    let type1: Type
+    let type2: Type
+    
+    init(type1: Type, type2: Type) {
+        self.type1 = type1
+        self.type2 = type2
+    }
+}
